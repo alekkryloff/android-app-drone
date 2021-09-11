15c82160134654590c018d717d7ccac3694db4d8
alekkryloff <alek.kryloff@yandex.ru>
2021-09-11 11:23:52 +0300

    Added JoystickActivity

---
+++ app/src/main/java/com/example/dronmanagement/JoystickActivity.java
@@ -0,0 +1,14 @@
+package com.example.dronmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoystickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joystick);
+    }
+}
