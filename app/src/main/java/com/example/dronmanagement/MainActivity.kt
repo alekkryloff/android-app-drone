d1af195cfbb5ae66188939eccf9012b562f6674b
alekkryloff <alek.kryloff@yandex.ru>
2021-09-11 11:22:53 +0300

    Added JoystickActivity

--- app/src/main/java/com/example/dronmanagement/MainActivity.kt
+++ app/src/main/java/com/example/dronmanagement/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.dronmanagement
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnJoystick).setOnClickListener {
+            startActivity(Intent(this, JoystickActivity::class.java))
+        }
     }
 }
